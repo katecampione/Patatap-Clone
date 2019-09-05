c0a94a7debebff4b39f354549c9487760a5fb602
Kate Campione <katecampione@gmail.com>
2019-09-05 16:43:50 -0600

    created color changing circle

--- js/Patatap.js
+++ js/Patatap.js
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>jQuery Todo List</title>
 
-		<link rel="stylesheet" type="text/css" href="Assets/css/Patatap.css"/>
-		<script type="text/javascript" src="Assets/js/jquery-3.4.1.min.js"></script>
-</head>
-<body>
-
-
-
-<script type="text/javascript" src="Assets/js/Patatap.js"></script>
-
-</body>
-
-</html>
